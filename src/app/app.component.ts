3fde58c02c9c09b41a037b282cb167e183b598de
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-19 17:44:58 -0300

    Criação do BrasilianStatesService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
 import { GenresService } from './services/genres.service';
+import { BrasilianStatesService } from './services/brasilian-states.service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,18 @@ import { GenresService } from './services/genres.service';
 export class AppComponent implements OnInit{
   usersList: any = [];
   genresList: any = [];
+  brasilianStatesList: any = [];
 
   constructor(
     private readonly _usersService: UsersService,
     private readonly _genresService: GenresService,
+    private readonly _brasilianStatesService: BrasilianStatesService,
   ){}
 
   ngOnInit(): void {
     this.getUsers();
     this.getGenres();
+    this.getStates();
   }
 
   private getUsers(){
@@ -30,6 +34,12 @@ export class AppComponent implements OnInit{
   private getGenres(){
     return this._genresService.getGenres().subscribe((genresListResponse) => {
       this.genresList = genresListResponse;
+    })
+  }
+
+  private getStates(){
+    return this._brasilianStatesService.getStates().subscribe((brasilianStatesListResponse) => {
+      this.brasilianStatesList = brasilianStatesListResponse;
     })
   }
 }
