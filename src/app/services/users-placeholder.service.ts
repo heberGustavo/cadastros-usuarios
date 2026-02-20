5fe99c3a40c46d0c0221f89dcc2b3964ed429d50
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-20 06:01:07 -0300

    Criação do UsersPlaceHolderService

---
+++ src/app/services/users-placeholder.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersPlaceholderService {
+
+  constructor(
+    private readonly _httpClient: HttpClient
+  ) {}
+
+  getUsersPlaceHolder(): Observable<any> {
+    return this._httpClient.get("https://jsonplaceholder.typicode.com/users");
+  }
+}
