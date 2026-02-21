87ff92ce1f999d349826c93f7b5017b2648d2971
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-21 09:26:15 -0300

    Criação e Aplicação do CredentialsValidator no Usuário e E-mail

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { EmailPatternValidatorDirective } from './email-pattern-validator.directive';
+import { CredentialsValidatorDirective } from './credentials-validator.directive';
 
 @NgModule({
     declarations: [
-    EmailPatternValidatorDirective
+    EmailPatternValidatorDirective,
+    CredentialsValidatorDirective
   ],
     exports: [
       EmailPatternValidatorDirective,
+      CredentialsValidatorDirective,
     ],
 })
 export class DirectivesModules {
