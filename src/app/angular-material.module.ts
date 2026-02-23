ed59de4fafe376349a7d11954d5ba432f1252c55
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-23 17:53:12 -0300

    Criação do combo de Estado

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -16,6 +17,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
         MatInputModule,
         MatProgressBarModule,
         MatNativeDateModule,
+        MatSelectModule,
     ],
     exports: [
         MatCardModule,
@@ -24,6 +26,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
         MatInputModule,
         MatProgressBarModule,
         MatDatepickerModule,
+        MatSelectModule,
     ],
 })
 export class AngularMaterialModule { }
